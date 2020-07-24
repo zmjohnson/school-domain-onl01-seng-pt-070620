67343f569f07925b5f0b5e44ac6c7b20142dea7a
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-24 13:18:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,25 @@
-# code here!
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
 
-class School
 
-attr_accessor :name, :roster
 
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
-
-  def add_student(student_name, grade)
-    if @roster[grade]
-      @roster[grade] << student_name
-    else
-      @roster[grade] = [student_name]
-    end
-  end
-
-  def grade(grade)
-    @roster[grade]
-  end
-
-  def sort
-    sorted_hash = {}
-    @roster.sort.each do |grade, name_array|
-      sorted_hash[grade] = (name_array.sort)
-    end
-    sorted_hash
-  end
-end
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("Moshe Zeus", 9)
+school.add_student("Onkelos Octavius", 9)
+school.roster
+school.add_student("Sugar Zaza", 5)
+school.roster
+school.add_student("Theo Madus", 5)
+school.add_student("Abbah Zabbah", 2)
+school.roster
+school.grade(9)
+school.sort
